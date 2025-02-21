891a814505f3beb4cb53086073bac1aab7279ea9
flsguedes <lucas.guedes08@aluno.ifce.edu.br>
2025-02-21 00:33:51 -0300

    :snake: Feat: Adicionando permissão CSRF a dominio

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://projectacademic-production.up.railway.app"
+]
