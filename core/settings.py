19a5a90396145aeff75deff4adf06618b564ee7e
flsguedes <lucas.guedes08@aluno.ifce.edu.br>
2025-02-21 00:28:51 -0300

    :snake: Feat: Adicionando Static

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from decouple import config
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +118,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,"home", "static")
+    ]
+
+STATIC_ROOT = BASE_DIR / "static" 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
